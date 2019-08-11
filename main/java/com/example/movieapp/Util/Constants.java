5278dca0c9ede3d5f4b581c84ae9249b3f6e8a90
Adith98SDE <53988049+Adith98SDE@users.noreply.github.com>
2019-08-11 10:10:58 +0530

    Update Constants.java

--- main/java/com/example/movieapp/Util/Constants.java
+++ main/java/com/example/movieapp/Util/Constants.java
@@ -3,6 +3,6 @@ package com.example.movieapp.Util;
 public class Constants {
     public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
     public static final String URL = "http://www.omdbapi.com/?i=";
-    public static final String API_KEY = "&apikey=d7f38721";
+    public static final String API_KEY = "&apikey=enteryourapikeyhere";      //enter apikey
     public static final String URL_RIGHT = "&page=2";
 }
